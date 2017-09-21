ea646470aae40337347c3f169536ec8e74d4176b
Michael Wanyoike <micwanyoike@gmail.com>
2017-09-21 23:34:11 +0300

    updated readme

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import store from './store.js';
 import { addToCart, updateCart, deleteFromCart }  from './actions/cart-actions';
 
-console.log("initial state: ", store.getState());
-
-let unsubscribe = store.subscribe(() =>
-  console.log(store.getState())
-);
+// console.log("initial state: ", store.getState());
+//
+// let unsubscribe = store.subscribe(() =>
+//   console.log(store.getState())
+// );
 
 // Add to Cart
 store.dispatch(addToCart('Coffee 500gm', 1, 250));
@@ -18,4 +18,6 @@ store.dispatch(updateCart('Flour 1kg', 5, 110));
 // Delete from Cart
 store.dispatch(deleteFromCart('Coffee 500gm'));
 
-unsubscribe();
+// unsubscribe();
+
+console.log('Install Redux DevTools Chrome Extension to view output!');
