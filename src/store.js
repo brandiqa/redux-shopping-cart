1662158555d4d8ef9ab9f9d25df29caaa9ef685f
Michael Wanyoike <micwanyoike@gmail.com>
2017-09-21 23:26:44 +0300

    setup Redux Devtools

--- src/store.js
+++ src/store.js
@@ -1,6 +1,7 @@
 import { createStore } from "redux";
+import { composeWithDevTools } from 'redux-devtools-extension';
 import rootReducer from './reducers';
 
-let store = createStore(rootReducer);
+const store = createStore(rootReducer, composeWithDevTools());
 
 export default store;
